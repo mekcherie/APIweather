5046c8f16f9f9caa53f2ed3cc9af7ae2c97523d9
Meklit Cherie <makywonde347@yahoo.com>
2021-05-06 20:01:08 -0700

    completed 6th and 7th challenges

--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
 // Functions 
-async function getWeather(zip, apiKey, units = 'imperial') {
+
+function getWeatherByZip(apiKey, zip, units = 'imperial') {
     const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
-    
+    return getWeather(path)
+}
+
+function getWeatherByCity(apiKey, city, units = 'imperial') {
+    const path = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`
+    return getWeather(path)
+}
+
+function getWeatherByGeo(apiKey, lat, lon, units = 'imperial') {
+    const path = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`
+    return getWeather(path)
+}
+async function getWeather(path) {
+    try { 
     const res = await fetch(path)
     const json = await res.json()
     const weatherData = {
         code: json.cod,
-        coord: json.coord,
-        tem: json.main.temp,
+        cordinates: json.coord,
+        temp: json.main.temp,
         feels_like: json.main.feels_like,
+        tem_min: json.main.temp_min,
         temp_max: json.main.temp_max,
         description: json.weather[0].description
     }
     return weatherData
-}  return fetch(path).then(res => res.json())
+}   catch(err) {
+    return err //handling the error
+    }
+}
 export {
-    getWeather 
+    getWeatherByZip,
+    getWeatherByCity,
+    getWeatherByGeo,
+    getWeather
 }
